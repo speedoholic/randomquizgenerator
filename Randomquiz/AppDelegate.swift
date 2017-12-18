2fd3948e89becd8625d5546229330d0a9a00ebe4
Kushal Ashok <kushal.ashok@gmail.com>
2017-12-19 00:29:36 +0800

    Initial Commit

---
+++ Randomquiz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Randomquiz
+//
+//  Created by Kushal Ashok on 12/19/17.
+//  Copyright © 2017 Essex Lake Group. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
